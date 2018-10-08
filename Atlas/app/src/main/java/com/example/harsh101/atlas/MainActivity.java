69ee1ac2e7b2af389affaf3d09e0b14c6c7f64a5
hsing013 <hsing013@ucr.edu>
2018-10-07 21:38:00 -0700

    Initial Setup for app

--- Atlas/app/src/main/java/com/example/harsh101/atlas/MainActivity.java
+++ Atlas/app/src/main/java/com/example/harsh101/atlas/MainActivity.java
@@ -3,13 +3,23 @@ package com.example.harsh101.atlas;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    enum State {initial_Login, initial_Signup, loginAttempt, signupAttempt, loggedIn}
+
+    public State state = State.initial_Login;
+
+    public Button login = findViewById(R.id.loginButton);
+    public TextView banner = findViewById(R.id.banner);
+    public TextView banner2 = findViewById(R.id.signUptext);
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,27 +28,42 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
             return false;
         }
     };
 
+    private void setFragment(Fragment frag){
+        FragmentTransaction fragTrans = getSupportFragmentManager().beginTransaction();
+        fragTrans.replace(R.id.mainFrame, frag);
+        fragTrans.commit();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    public void signUpClick(View v){
+        if (state == State.initial_Login){
+            login.setText("Signup");
+            banner.setText("Signup here!");
+            banner2.setText("Back to the Login?");
+        }
+    }
+
+    public void loginButtonClick(View v){
+
     }
 
 }
