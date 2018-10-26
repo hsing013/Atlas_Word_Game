57598b9e64ed4893f5c75c1bb01df6734b6a6cf5
Yichao Chen <ychen177@ucr.edu>
2018-10-26 13:31:02 -0700

    NotificationFrag update (not tested yet)

--- src/Atlas/app/src/main/java/com/example/harsh101/atlas/NotificationFrag.java
+++ src/Atlas/app/src/main/java/com/example/harsh101/atlas/NotificationFrag.java
@@ -19,11 +19,26 @@ public class NotificationFrag extends Fragment {
     }
 
 
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_notification, container, false);
+
+        /**
+         * BottomNavigationMenuView bottomNavigationMenuView =
+         *             (BottomNavigationMenuView) navigationView.getChildAt(0);
+         *     View v = bottomNavigationMenuView.getChildAt(3);
+         *     BottomNavigationItemView itemView = (BottomNavigationItemView) v;
+         *
+         *     View badge = LayoutInflater.from(this)
+         *             .inflate(R.layout.notification_badge, bottomNavigationMenuView, false);
+         *
+         *     itemView.addView(badge);
+         */
+        /
     }
 
 }
