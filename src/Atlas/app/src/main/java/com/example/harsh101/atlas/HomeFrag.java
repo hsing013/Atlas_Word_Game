b47ce5305acd211dda796e89fd90113ba5e79394
Yichao Chen <ychen177@ucr.edu>
2018-11-26 00:43:26 -0800

    Overall UI changes

--- src/Atlas/app/src/main/java/com/example/harsh101/atlas/HomeFrag.java
+++ src/Atlas/app/src/main/java/com/example/harsh101/atlas/HomeFrag.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -23,6 +24,7 @@ public class HomeFrag extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
